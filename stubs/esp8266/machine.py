ab6fbc3cb68658a62090939b984bb8b57ced3f95
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2021-02-14 18:55:26 +0000

    added docstrings and params to I2C class

--- stubs/esp8266/machine.py
+++ stubs/esp8266/machine.py
@@ -4,6 +4,8 @@ Module: 'machine' on micropython-esp8266-2.0.0(5a875ba)
 # MCU: {'ver': '2.0.0(5a875ba)', 'port': 'esp8266', 'arch': 'xtensa', 'sysname': 'esp8266', 'release': '2.0.0(5a875ba)', 'name': 'micropython', 'mpy': 9733, 'version': '1.14.0', 'machine': 'ESP module with ESP8266', 'build': '', 'nodename': 'esp8266', 'platform': 'esp8266', 'family': 'micropython'}
 # Stubber: 1.3.9
 
+from random import randint
+
 class ADC:
     ''
     def read():
@@ -17,45 +19,104 @@ DEEPSLEEP_RESET = 5
 HARD_RESET = 6
 
 class I2C:
-    ''
-    def init():
+    '''The I2C driver is implemented in software and works on all pins'''
+    
+    def init(self, scl, sda, *, freq=400000, timeout=255):
+        '''
+        Construct a new software I2C object.
+
+        param
+
+        scl - pin object specifying the pin to use for SCL.
+        sda - pin object specifying the pin to use for SDA.
+        freq - integer which sets the maximum frequency for SCL.
+        timeout - the maximum time in microseconds to wait for clock stretching (SCL held low by another device on the bus), after which an OSError(ETIMEDOUT) exception is raised.
+        '''
         pass
 
-    def readfrom():
+    def readfrom(self, addr, nbytes, stop=True, /):
+        '''
+        Read nbytes from the slave specified by addr. If stop is true then a STOP condition is generated at the end of the transfer.
+        
+        Returns a bytes object with the data read.
+        '''
+        return [randint(0, 2**8-1)]*nbytes
+
+    def readfrom_into(self, addr, buf, stop=True, /):
+        '''
+        Read into buf from the slave specified by addr. The number of bytes read will be the length of buf. If stop is true then a STOP condition is generated at the end of the transfer.
+
+        Returns None.
+        '''
+        return None
+
+    def readfrom_mem(self, addr, memaddr, nbytes, *, addrsize=8):
+        '''
+        Read nbytes from the slave specified by addr starting from the memory address specified by memaddr. The argument addrsize specifies the address size in bits.
+        
+        Returns a bytes object with the data read.
+        '''
+        return [randint(0, 2**8-1)]*nbytes
+
+    def readfrom_mem_into(self, addr, memaddr, buf, *, addrsize=8):
+        '''
+        Read into buf from the slave specified by addr starting from the memory address specified by memaddr. The number of bytes read is the length of buf. The argument addrsize specifies the address size in bits (on ESP8266 this argument is not recognised and the address size is always 8 bits).
+
+        Returns None.
+        '''
+        return None
+
+    def readinto(self, buf, nack=True, /):
+        '''
+        Reads bytes from the bus and stores them into buf. The number of bytes read is the length of buf. An ACK will be sent on the bus after receiving all but the last byte. After the last byte is received, if nack is true then a NACK will be sent, otherwise an ACK will be sent (and in this case the slave assumes more bytes are going to be read in a later call).
+        '''
         pass
 
-    def readfrom_into():
+    def scan(self):
+        '''Scan all I2C addresses between 0x08 and 0x77 inclusive. Returns a list of those that respond.'''
         pass
 
-    def readfrom_mem():
+    def start(self):
+        '''Generate a START condition on the bus (SDA transitions to low while SCL is high).'''
         pass
 
-    def readfrom_mem_into():
+    def stop(self):
+        '''Generate a STOP condition on the bus (SDA transitions to high while SCL is high).'''
         pass
 
-    def readinto():
-        pass
+    def write(self, buf):
+        '''
+        Write the bytes from buf to the bus. Checks that an ACK is received after each byte and stops transmitting the remaining bytes if a NACK is received.
+        
+        Returns the number of ACKs that were received.
+        '''
+        return len(buf)
 
-    def scan():
-        pass
+    def writeto(self, addr, buf, stop=True, /):
+        '''
+        Write the bytes from buf to the slave specified by addr. If a NACK is received following the write of a byte from buf then the remaining bytes are not sent. If stop is true then a STOP condition is generated at the end of the transfer, even if a NACK is received.
+        
+        Returns the number of ACKs that were received.
+        '''
+        return len(buf)
 
-    def start():
-        pass
+    def writeto_mem(self, addr, memaddr, buf, *, addrsize=8):
+        '''
+        Write buf to the slave specified by addr starting from the memory address specified by memaddr. The argument addrsize specifies the address size in bits (on ESP8266 this argument is not recognised and the address size is always 8 bits).
 
-    def stop():
-        pass
+        Returns None.
+        '''
+        return None
 
-    def write():
-        pass
+    def writevto(self, addr, vector, stop=True, /):
+        '''
+        Write the bytes contained in vector to the slave specified by addr. vector should be a tuple or list of objects with the buffer protocol. The addr is sent once and then the bytes from each object in vector are written out sequentially. The objects in vector may be zero bytes in length in which case they don’t contribute to the output.
 
-    def writeto():
-        pass
-
-    def writeto_mem():
-        pass
-
-    def writevto():
-        pass
+        If a NACK is received following the write of a byte from one of the objects in vector then the remaining bytes, and any remaining objects, are not sent. If stop is true then a STOP condition is generated at the end of the transfer, even if a NACK is received.
+        
+        Returns the number of ACKs that were received.
+        '''
+        return len(vector)
 
 
 class PWM:
